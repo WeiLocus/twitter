bb6aba66379895b9fa41a400ebde5643edcfdab3
Hsin-tingCHI <chihsinting@gmail.com>
2023-03-20 09:45:45 +0100

    layout: add basic navlinks

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,3 +1,38 @@
+import { NavLink, useLocation } from 'react-router-dom';
+import styled from 'styled-components';
+import { ReactComponent as LogoIcon } from '../assets/Logo.svg';
+
+const StyledNav = styled.nav`
+  height: 100vh;
+  background-color: white;
+`;
+
 export default function Navbar() {
-  return <div>Navbar</div>;
+  const { pathname } = useLocation();
+  const user = { id: 5 }; // 暫時假定
+
+  return (
+    <StyledNav>
+      <div>Navbar</div>
+      <div>
+        <LogoIcon />
+      </div>
+      <ul>
+        <li>
+          <NavLink to="/tweets">首頁</NavLink>
+        </li>
+        <li>
+          <NavLink
+            to={`/users/${user.id}/tweets`}
+            className={pathname.includes(`users/${user.id}`) && 'active'}
+          >
+            個人資料
+          </NavLink>
+        </li>
+        <li>
+          <NavLink to="/settings">設定</NavLink>
+        </li>
+      </ul>
+    </StyledNav>
+  );
 }
