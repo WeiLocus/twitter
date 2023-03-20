9cbb4f5b4d6f405bfc753f9502500ad093268e3c
Hsin-tingCHI <chihsinting@gmail.com>
2023-03-20 10:54:05 +0100

    layout: add Navbar basic desktop styles

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,10 +1,55 @@
 import { NavLink, useLocation } from 'react-router-dom';
 import styled from 'styled-components';
 import { ReactComponent as LogoIcon } from '../assets/Logo.svg';
+import { ReactComponent as HomeIcon } from '../assets/Home.svg';
+import { ReactComponent as PersonIcon } from '../assets/Person.svg';
+import { ReactComponent as SettingIcon } from '../assets/Setting.svg';
+import { ReactComponent as LogoutIcon } from '../assets/Logout.svg';
 
 const StyledNav = styled.nav`
   height: 100vh;
+  display: flex;
+  flex-direction: column;
+  justify-content: space-between;
+  padding: 0.5rem 1rem;
   background-color: white;
+
+  .logo {
+    margin-bottom: 0.25rem;
+  }
+`;
+
+const StyledLi = styled.li`
+  display: flex;
+  gap: 1rem;
+  padding: 0.75rem 0.5rem;
+  font-weight: 700;
+  color: var(--color-gray-800);
+
+  .icon {
+    width: 15%;
+    display: grid;
+    place-items: center;
+  }
+
+  .active & {
+    color: var(--color-theme);
+  }
+
+  :hover {
+    color: var(--color-gray-700);
+  }
+`;
+
+const StyledNavButton = styled.button`
+  cursor: pointer;
+  width: 100%;
+  margin: 1rem 0;
+  padding: 0.5rem 0;
+  border: none;
+  border-radius: 2rem;
+  color: white;
+  background-color: var(--color-theme);
 `;
 
 export default function Navbar() {
@@ -13,25 +58,51 @@ export default function Navbar() {
 
   return (
     <StyledNav>
-      <div>Navbar</div>
       <div>
-        <LogoIcon />
-      </div>
-      <ul>
-        <li>
-          <NavLink to="/tweets">首頁</NavLink>
-        </li>
-        <li>
+        <div className="logo">
+          <LogoIcon />
+        </div>
+        <ul>
+          <NavLink to="/tweets">
+            <StyledLi>
+              <div className="icon">
+                <HomeIcon />
+              </div>
+              <span>首頁</span>
+            </StyledLi>
+          </NavLink>
           <NavLink
             to={`/users/${user.id}/tweets`}
             className={pathname.includes(`users/${user.id}`) && 'active'}
           >
-            個人資料
+            <StyledLi>
+              <div className="icon">
+                <PersonIcon />
+              </div>
+
+              <span>個人資料</span>
+            </StyledLi>
           </NavLink>
-        </li>
-        <li>
-          <NavLink to="/settings">設定</NavLink>
-        </li>
+
+          <NavLink to="/settings">
+            <StyledLi>
+              <div className="icon">
+                <SettingIcon />
+              </div>
+
+              <span>設定</span>
+            </StyledLi>
+          </NavLink>
+        </ul>
+        <StyledNavButton>推文</StyledNavButton>
+      </div>
+      <ul>
+        <StyledLi>
+          <div className="icon">
+            <LogoutIcon />
+          </div>
+          <span>登出</span>
+        </StyledLi>
       </ul>
     </StyledNav>
   );
