c41c1dc2d9857a0703902fc00851ccbc52dadf48
Hsin-tingCHI <chihsinting@gmail.com>
2023-03-21 22:23:41 +0100

    layout: add admin container

--- src/components/layouts/AdminLayout.jsx
+++ src/components/layouts/AdminLayout.jsx
@@ -1,5 +1,27 @@
-import React from 'react';
+import styled from 'styled-components';
+import { Outlet } from 'react-router-dom';
+import { GridContainer, device } from '../../globalStyles';
+import Navbar from '../Navbar';
+
+const AdminContainer = styled(GridContainer)`
+  @media screen and (${device.md}) {
+    grid-template-columns: 1fr 7fr;
+  }
+
+  @media screen and (${device.lg}) {
+    grid-template-columns: 1fr 4.5fr;
+  }
+`;
 
 export default function AdminLayout() {
-  return <div>AdminLayout</div>;
+  return (
+    <AdminContainer>
+      <div className="fr1">
+        <Navbar />
+      </div>
+      <div className="fr2">
+        <Outlet />
+      </div>
+    </AdminContainer>
+  );
 }
