8057d82f178d0ba637e0c4bca3d73f42c377bd0a
Hsin-tingCHI <chihsinting@gmail.com>
2023-03-21 11:29:55 +0100

    layout: add profile styles

--- src/pages/UserPage.jsx
+++ src/pages/UserPage.jsx
@@ -1,12 +1,14 @@
+import { useParams } from 'react-router-dom';
 import Header from '../components/Header';
 import Profile from '../components/Profile';
 import TweetList from '../components/TweetList';
 
 export default function UserPage() {
+  const { id } = useParams();
   return (
     <>
       <Header headerText="John Doe" goBack user />
-      <Profile />
+      <Profile id={id} />
       <TweetList />
     </>
   );
