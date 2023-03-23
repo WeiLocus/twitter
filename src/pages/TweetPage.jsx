88e1d5d4e0be82f952871fed0f9065b38e9cf608
WeiLocus <marsuo52@gmail.com>
2023-03-23 21:48:35 +0800

    feat: test tweets api

--- src/pages/TweetPage.jsx
+++ src/pages/TweetPage.jsx
@@ -1,4 +1,6 @@
 import styled from 'styled-components';
+import axios from 'axios';
+import { useEffect } from 'react';
 import Header from '../components/Header';
 import TweetInput from '../components/TweetInput';
 import TweetList from '../components/TweetList';
@@ -7,8 +9,27 @@ const StyledDiv = styled.div`
   height: calc(100vh - 68px);
   overflow-y: scroll;
 `;
+const baseURL = `https://murmuring-plains-40389.herokuapp.com/api`;
 
+async function getTweets(token) {
+  try {
+    const response = await axios.get(`${baseURL}/tweets`, {
+      headers: {
+        Authorization: 'Bearer ' + token,
+      },
+    });
+    console.log(response.data);
+  } catch (error) {
+    console.error(error);
+  }
+}
 export default function TweetPage() {
+  useEffect(() => {
+    const token = localStorage.getItem('token');
+    if (token) {
+      getTweets(token);
+    }
+  }, []);
   return (
     <>
       <Header headerText="首頁" />
