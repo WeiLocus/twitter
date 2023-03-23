855b224787d7d6537dd9c56a14c4a1e395e2e405
WeiLocus <marsuo52@gmail.com>
2023-03-23 19:14:41 +0800

    feat: add login api function

---
+++ src/api/auth.js
@@ -0,0 +1,25 @@
+import axios from 'axios';
+
+const authURL = 'https://murmuring-plains-40389.herokuapp.com/api';
+
+// eslint-disable-next-line consistent-return
+const login = async ({ account, password }) => {
+  try {
+    const { data } = await axios.post(`${authURL}/users/signin`, {
+      account,
+      password,
+    });
+
+    // 如果註冊成功，拿得到token
+    const { token } = data.data;
+
+    // 判斷是否登入要回傳的內容
+    if (token) {
+      return { success: true, ...data };
+    }
+    return data;
+  } catch (error) {
+    console.log('[Login Failed]:', error);
+  }
+};
+export default login;
