e30f061912004866c6bf1eb6759c2571123c2ed8
Hsin-tingCHI <chihsinting@gmail.com>
2023-03-24 16:37:31 +0100

    feat: add user.js file

---
+++ src/api/user.js
@@ -0,0 +1,21 @@
+/* eslint-disable import/no-extraneous-dependencies */
+import axios from 'axios';
+
+const baseURL = 'https://murmuring-plains-40389.herokuapp.com/api';
+
+const axiosInstance = axios.create({
+  baseURL,
+});
+
+axiosInstance.interceptors.request.use(
+  (config) => {
+    const token = localStorage.getItem('token');
+    if (token) {
+      config.headers['Authorization'] = `Bearer ${token}`;
+    }
+    return config;
+  },
+  (error) => {
+    console.error(error);
+  }
+);
