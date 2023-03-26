211fee726e24909fe9d0855ec7aadb7807ebe9a2
Hsin-tingCHI <chihsinting@gmail.com>
2023-03-26 15:25:16 +0200

    feat: add followship.js & like.js file

---
+++ src/api/like.js
@@ -0,0 +1,43 @@
+/* eslint-disable import/no-extraneous-dependencies */
+import axios from 'axios';
+
+const baseURL = 'https://murmuring-plains-40389.herokuapp.com/api';
+
+const axiosInstance = axios.create({
+  baseURL,
+});
+
+axiosInstance.interceptors.request.use(
+  (config) => {
+    const token = localStorage.getItem('token');
+    if (token) {
+      config.headers['Authorization'] = `Bearer ${token}`;
+    }
+    return config;
+  },
+  (error) => {
+    console.error(error);
+  }
+);
+
+// * 將特定推文加入喜歡
+export async function likeTweet(id) {
+  try {
+    const res = await axiosInstance.post(`${baseURL}/tweets/${id}/like`);
+    console.log(res.data);
+    return res.data;
+  } catch (error) {
+    console.error('[Like Tweet failed]: ', error);
+  }
+}
+
+// * 將特定推文移除喜歡
+export async function unLikeTweet(id) {
+  try {
+    const res = await axiosInstance.post(`${baseURL}/tweets/${id}/unlike`);
+    console.log(res.data);
+    return res.data;
+  } catch (error) {
+    console.error('[Unlike Tweet failed]: ', error);
+  }
+}
